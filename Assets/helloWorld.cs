5b7d9a466e2eb6bcc766769e98fa7567622abe20
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-01-11 21:06:42 -0800

    Initial commit

---
+++ Assets/helloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class helloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        print("ello allo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
